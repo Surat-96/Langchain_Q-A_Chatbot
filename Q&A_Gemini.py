d1dc03cd7962aeecfbf87ecce30ba17174cc4ac7
Surat Banerjee <67864248+Surat-96@users.noreply.github.com>
2024-06-29 23:24:27 +0530

    Update Q&A_Gemini.py

--- Q&A_Gemini.py
+++ Q&A_Gemini.py
@@ -36,3 +36,30 @@ if submit:
     response=get_gemini_response(input)
     st.subheader("The Response is")
     st.write(response)
+## streamlit app
+st.set_page_config("Chat With Multiple PDF")
+st.header("Chat with Multiple PDF using Gemini :books:")
+
+user_question = st.text_input("Ask a Question from the PDF Files")
+
+if user_question:
+    user_input(user_question)
+
+with st.sidebar:
+    st.title("Menu:")
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+    if st.button("Submit & Process"):
+        with st.spinner("Processing..."):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            get_vector_store(text_chunks)
+            st.success("Done")
+
+
+footer = """
+---
+#### Made By [Surat Banerjee](https://www.linkedin.com/in/surat-banerjee/)
+For Any Queries, Reach out on [Portfolio](https://suratbanerjee.wixsite.com/myportfoliods)  
+"""
+
+st.markdown(footer, unsafe_allow_html=True)
