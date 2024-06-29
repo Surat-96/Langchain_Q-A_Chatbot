58cb9c73c0207874bbce08485f8bd87fa3b98d93
Surat Banerjee <67864248+Surat-96@users.noreply.github.com>
2024-06-29 23:29:17 +0530

    Update Con_Q&A_OpenAI.py

--- Con_Q&A_OpenAI.py
+++ Con_Q&A_OpenAI.py
@@ -39,25 +39,6 @@ if submit:
     st.subheader("The Response is")
     st.write(response)
 
-## streamlit app
-st.set_page_config("Chat With Multiple PDF")
-st.header("Chat with Multiple PDF using Gemini :books:")
-
-user_question = st.text_input("Ask a Question from the PDF Files")
-
-if user_question:
-    user_input(user_question)
-
-with st.sidebar:
-    st.title("Menu:")
-    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-    if st.button("Submit & Process"):
-        with st.spinner("Processing..."):
-            raw_text = get_pdf_text(pdf_docs)
-            text_chunks = get_text_chunks(raw_text)
-            get_vector_store(text_chunks)
-            st.success("Done")
-
 
 footer = """
 ---
